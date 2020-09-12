a83478921f20e6cfc1d9e2a45709438fae8ebbe9
Skalice132 <58006458+Skalice132@users.noreply.github.com>
2020-09-13 01:35:51 +0400

    add .htaccess

--- index.php
+++ index.php
@@ -5,17 +5,43 @@
 </head>
 <body>
 HELLO WORLD
+<form action="" method="POST">
+
+	<!-- ФИО  -->
+	<p>
+		<label>Введите Ваше имя:</label><br>
+		<input type="text" id="" name="user_name" placeholder="Иванов Иван Иванович">
+	</p>
+	<!-- Возраст -->
+	<p>
+		<label>Укажите дату рождения:</label><br>
+		<input type="date" name="date"/><br> 
+	</p>
+
+	<!-- Город -->
+	<p>
+		<label>Укажите город:</label><br>
+		<input type="text" name="user_sity" placeholder="Samara">
+	</p>
+
+	<!-- Контакты -->
+	<p>
+		<label>Напишите свой номер:</label><br>
+		<input type="text" id="" name="user_phone" placeholder="+7 (999) 99 99 999"><br> 
+		<label>Напишите свой email:</label><br>
+		<input type="mail" id="" name="user_email" placeholder="example@mail.ru">
+	</p>
+<br>
+	<button type="submit" >Отправить форму</button>
+</form>
 
 <?php
 
 // FIO
 $name = $_POST['user_name'];
-$surname = $_POST['user_surname'];
 
 // age
-$year = $_POST['user_year'];
-$month = $_POST['user_month'];
-$day = $_POST['user_day'];
+$date = $_POST['date'];
 
 // sity
 $sity = $_POST['user_sity'];
@@ -23,7 +49,6 @@ $sity = $_POST['user_sity'];
 // contact
 $phone = $_POST['user_phone'];
 $email = $_POST['user_email'];
-
 
 ?>
 </body>
